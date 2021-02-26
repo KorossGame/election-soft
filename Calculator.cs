aace24df88523e7ad2d4921778ee683e601f7cad
Oleg Jakovlev <oleg27132486@gmail.com>
2021-02-26 17:58:30 +0000

    Fixed issues

--- Calculator.cs
+++ Calculator.cs
@@ -1,23 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace ElectionSoft
 {
     static class Calculator
     {
         private static Party winningParty;
+        private static int counter = 0;
 
-        public static void Calculate(int seatsToAllocate, Party[] parties)
+        public static void Calculate(int seatsToAllocate, List<Party> parties)
         {
             for (int seat = seatsToAllocate; seat > 0; seat--)
             {
                 winningParty = GetPartyWithMaxVotes(parties);
                 IncreaseMEPCount(winningParty);
             }
+
+            foreach (Party party in parties)
+            {
+                Console.WriteLine(party.PartyName+"\t"+party.MEPCount);
+            }
         }
 
-        private static Party GetPartyWithMaxVotes(Party[] parties)
+        private static Party GetPartyWithMaxVotes(List<Party> parties)
         {
             int maxVotes = -1;
             winningParty = null;
@@ -36,7 +43,9 @@ namespace ElectionSoft
 
         private static void IncreaseMEPCount(Party winningParty)
         {
+            counter++;
             winningParty.MEPCount++;
+            winningParty.NumberOfVotes /= (counter + 1);
         }
     }
 }
